9f251dccfd6ea967b5ac343562bba0aa8a5fa281
hellytking <hellytking788@gmail.com>
2026-05-02 19:09:12 +0530

    fix port for deployment

--- backend/server.js
+++ backend/server.js
@@ -19,4 +19,5 @@ app.get("/", (req, res) => {
   res.send("API working")
 })
 
-app.listen(5000, () => console.log("Server running"))
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log("Server running on port " + PORT))
